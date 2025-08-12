1b18e37f797de6be216542300c46b133b5d91918
hannayehoshynanwp-cmyk <hanna.yehoshynanwp@gmail.com>
2025-08-11 21:42:24 -0600

    Update NBDC_bees_IDsApp_v1.py

--- NBDC_bees_IDsApp_v1.py
+++ NBDC_bees_IDsApp_v1.py
@@ -1,17 +1,6 @@
 import streamlit as st
-import pickle
-
-import numpy as np
-import pandas as pd
-
-#import tensorflow as tf
 
 from PIL import Image
-
-#from tensorflow.keras.preprocessing import image
-
-#import plotly.express as px
-
 
 
 
@@ -31,81 +20,12 @@ def main():
   else:
     st.info("Please upload an image file.")
   
-  #st.write('Enter the filename to predict its Bee species')
-  # User input
-  #beeImgFile=st.text_area('', height=200)
-  # Button to make a prediction
-  '''if st.button('Predict'):
-        try:
-          uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'png', 'jpeg'])
-          if uploaded_file is not None:
-            try:
-              st.write("Displaying...")
-              image = Image.open(uploaded_file)
-              st.image(image, caption='Uploaded Image of Bee', use_column_width=True)
-            except Exception as e:
-              st.error(f"Error loading image: {e}")'''
-              
-            
-            
-            '''beeImgFile=preprocess_img(uploaded_file)
-
-            st.write("")
-            st.write("Identification...")
-
-            model = load_model('model1_v3_NDBC_Bees_8_8_25.pkl')
-            #Load labels
-            labels=pickle.load(open('model1_v3_NDBC_Bees_8_8_25_labels.pkl', 'rb'))
-            # Make a prediction
-            testImgPreds=model.predict(preprocess_img(beeImgFile))
-            # Display the result
-            st.success(f"Top class is: {labels[testImgPreds[0].argmax()]}")
-            # Visualization
-            display_predictions(testImgPreds,labels)
-          else:
-            st.error(f"An unexpected error occurred :(")'''
-
-
-
-        except Exception as e:
-          st.error(f"Error in prediction: {e}")
-            # Load your model (with caching to improve performance)
-
-  @st.cache_resource()
-  def load_model(model_file):
-      model = pickle.load(open(model_file, 'rb'))
-      return model
-
-  
-  def display_predictions(preds, labels):
-    st.warning('Predictions...')
-    species=[]
-    specPreds=[]    
-    for index, pred in enumerate(preds.flatten()):
-      st.write(f'{labels[index]}: {pred:.2f}%')
-      species.append(labels[index])
-      specPreds.append(pred)
-    pred_df=pd.DataFrame()
-    pred_df['Species']=species
-    pred_df['Pred']=specPreds
-
-    fig = px.bar(pred_df, x="Pred", y="Species", orientation='h',height=800, width=1000, title='NDBC Bees Identification')
-    st.plotly_chart(fig)
-
-  
-  #Img preprocessing
-  def preprocess_img(beeImgFile, IMG_SIZE=416):
-    st.warning('Preprocessing...')
-
-    rawImg=image.load_img(beeImgFile, target_size=(IMG_SIZE,IMG_SIZE))
-    imgArr=image.img_to_array(rawImg)
-    imgArrExp = np.expand_dims(imgArr, 0)
-    beeImg=tf.keras.applications.mobilenet_v3.preprocess_input(imgArrExp)
-    return beeImg
+ 
 
 if __name__ == '__main__':
 
     main()
+
 
 
 
