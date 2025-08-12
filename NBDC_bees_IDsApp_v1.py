82e8b6c93800192b0dc923f36bfda9c0aaa748d8
hannayehoshynanwp-cmyk <hanna.yehoshynanwp@gmail.com>
2025-08-11 22:02:28 -0600

    Update NBDC_bees_IDsApp_v1.py

--- NBDC_bees_IDsApp_v1.py
+++ NBDC_bees_IDsApp_v1.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 import tensorflow as tf
 
+import joblib
+
 from tensorflow.keras.preprocessing import image
 
 import plotly.express as px
@@ -14,6 +16,19 @@ import plotly.express as px
 
 
 def main():
+  #Img preprocessing
+  def preprocess_img(beeImgFile, IMG_SIZE=416):
+    st.info('Preprocessing...')
+    rawImg=image.load_img(beeImgFile, target_size=(IMG_SIZE,IMG_SIZE))
+    imgArr=image.img_to_array(rawImg)
+    imgArrExp = np.expand_dims(imgArr, 0)
+    beeImg=tf.keras.applications.mobilenet_v3.preprocess_input(imgArrExp)
+    return beeImg
+    
+  def load_model(model_file):
+    model = joblib.load(model_file)
+    return model
+    
   # Set up your web app
 
   st.title('NDBC Bees Identification with MobileNetV3')
@@ -26,14 +41,31 @@ def main():
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image')
     st.success("Image uploaded successfully!")
+    
+    # Button to make a prediction
+    if st.button('Predict'):
+      try:
+        beeImgFile=preprocess_img(uploaded_file)
+        st.write("")
+        st.write("Identification...")
+        model = load_model('model1_v3_NDBC_Bees_8_8_25.pkl')
+        labels=joblib.load('model1_v3_NDBC_Bees_8_8_25_labels.pkl')
+        testImgPreds=model.predict(beeImgFile)
+        st.success(f"Top class is: {labels[testImgPreds[0].argmax()]}")
+      except Exception as e:
+          st.error(f"Error in prediction: {e}")
+
+  
   else:
     st.info("Please upload an image file.")
+    
   
  
 
 if __name__ == '__main__':
 
     main()
+
 
 
 
