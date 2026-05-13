82244c4632ede004bbe33fc59e86174c04d8c60c
Hanna-Yehoshyna-NWP <hanna.yehoshynanwp@gmail.com>
2026-05-13 14:08:55 -0600

    Update NBDC_bees_IDsApp_v1.py

--- NBDC_bees_IDsApp_v1.py
+++ NBDC_bees_IDsApp_v1.py
@@ -118,7 +118,7 @@ def main():
                 model = load_model('smoteImgBees17genus_classification_v3_model_1FullLarge1270adasynCW_9_5_26_32_416.pkl')
                 labels=joblib.load('smoteImgBees17genus_classification_v3_LABELS_1FullLarge1270adasynCW_9_5_26_32_416.pkl')
                 testImgPreds=model.predict(beeImgFile)
-                st.success(f"Top class is: {labels[testImgPreds[0].argmax()]}")
+                st.success(f"Top class is: {labels[str(testImgPreds[0].argmax())]}")
                 display_predictions(testImgPreds,labels)
                 
             except Exception as e:
